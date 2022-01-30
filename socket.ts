2f13f457e3e6119e838bbf894c944bc315c429e7
BeWoler <bewoler.del@gmail.com>
2022-01-30 03:41:49 +0300

    refactoring

--- socket.ts
+++ socket.ts
@@ -6,6 +6,13 @@ dotenv.config();
 const app = express();
 const server = require("http").Server(app);
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,POST");
+  next();
+});
+
 const io = require("socket.io")(server, {
   cors: {
     origin: process.env.CORS_ORIGIN,
